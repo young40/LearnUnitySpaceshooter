bfe6de78aaab6dee6ce98ce5695e3444f17b6d25
Peter Young <young40@gmail.com>
2017-05-26 17:26:52 +0800

    update

---
+++ Assets/Script/BoxField.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxField : MonoBehaviour {
+
+	void OnTriggerExit(Collider other){
+		Destroy (other.gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
