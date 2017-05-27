1e2f48631f12b13edf037132ab1113a6786c6c19
Peter Young <young40@gmail.com>
2017-05-27 15:51:10 +0800

    update

---
+++ Assets/Script/DestoryByContact.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryByContact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Box")) {
+			return;
+		}
+
+		Destroy (this.gameObject);
+		Destroy (other.gameObject);
+	}
+}
